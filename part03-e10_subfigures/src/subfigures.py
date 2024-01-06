a56577ac715a84bee7146751b5c4174f73a389b2
sagarpoojary <poojarysagar09@gmail.com>
2024-01-06 20:26:14 +0530

    Complete: Matplotlib

--- part03-e10_subfigures/src/subfigures.py
+++ part03-e10_subfigures/src/subfigures.py
@@ -4,10 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def subfigures(a):
-    pass
+    plt.subplot(1,2,1)
+    plt.plot(a[:, 0], a[:, 1])
+    plt.subplot(1,2,2)
+    plt.scatter(a[:, 0], a[:, 1], c=a[:, 2], s=a[:, 3])
+    plt.show()
 
 def main():
-    pass
+    np.random.seed(0)
+    a = np.random.randint(1,100, (4,4))
+    print(a)
+    subfigures(a)
+
 
 if __name__ == "__main__":
     main()
